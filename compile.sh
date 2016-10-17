0b65c31c1e3a5d740a34c1ce912fd0fa459c732a
solohin <solohin.i@gmail.com>
2016-10-17 17:27:52 +0300

    Add chrome manual test

--- compile.sh
+++ compile.sh
@@ -28,3 +28,6 @@ echo "### PUSH HTML COMPLETE            ###";
 echo "#####################################";
 
 cd /Users/solohin/Sites/data5_github;
+
+/usr/bin/open -a "/Applications/Google Chrome.app" 'https://data5.pro/';
+/usr/bin/open -a "/Applications/Google Chrome.app" 'https://github.com/solohin/data5.pro/settings/';
