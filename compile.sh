d45051aeeafaa58468e3c607e2ff5d7235ab4171
solohin <solohin.i@gmail.com>
2016-10-17 16:16:33 +0300

    Test with sh

--- compile.sh
+++ compile.sh
@@ -17,6 +17,7 @@ echo "#####################################";
 rm -rf /Users/solohin/Sites/data5_github_pages/*;
 cp -R /Users/solohin/Sites/data5_github/build/* /Users/solohin/Sites/data5_github_pages;
 cd /Users/solohin/Sites/data5_github_pages/;
+git pull;
 git add .;
 git commit -m "$1" ;
 git push ;
