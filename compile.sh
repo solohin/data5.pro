ecf235c477ea7b4564ecde70be34bc93cee0b384
solohin <solohin.i@gmail.com>
2016-10-17 16:16:10 +0300

    Test with sh

---
+++ compile.sh
@@ -0,0 +1,28 @@
+cd /Users/solohin/Sites/data5_github;
+
+git add .;
+git commit -m "$1" ;
+git push ;
+
+echo "#####################################";
+echo "### PUSH SOURCE COMPLETE          ###";
+echo "#####################################";
+
+spress site:build;
+
+echo "#####################################";
+echo "### BUILD COMPLETE                ###";
+echo "#####################################";
+
+rm -rf /Users/solohin/Sites/data5_github_pages/*;
+cp -R /Users/solohin/Sites/data5_github/build/* /Users/solohin/Sites/data5_github_pages;
+cd /Users/solohin/Sites/data5_github_pages/;
+git add .;
+git commit -m "$1" ;
+git push ;
+
+echo "#####################################";
+echo "### PUSH HTML COMPLETE            ###";
+echo "#####################################";
+
+cd /Users/solohin/Sites/data5_github;
